0ccc9cbe35505a54f17484bf26ff60e2bc5290b0
Алексей Шербаков <leha.gb.2023@mail.ru>
2025-01-07 09:55:16 +1000

    Изменил движение такси

--- HomeWork_Unity/HomeWork1/Assets/Scripts/MoveCar.cs
+++ HomeWork_Unity/HomeWork1/Assets/Scripts/MoveCar.cs
@@ -6,12 +6,14 @@ public class MoveCar : MonoBehaviour
 {
     public float speed = 2f;
     private float maxSpeed = 6f;
-    private float upperBound = 47f;
+    private float upperBoundZ = 47f;
+    private float upperBoundX = 48f;
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        RotateCar();
     }
     public void Move()
     {
@@ -20,7 +22,20 @@ public class MoveCar : MonoBehaviour
         transform.position += moveDirection * Random.Range(speed, maxSpeed) * Time.deltaTime;
 
         // Удаление автомобиля.
-        if (transform.position.z >= upperBound)
+        if (transform.position.z >= upperBoundZ)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void RotateCar()
+    {
+        Vector3 moveDirection = transform.forward;
+        if (CompareTag("Taxi") && transform.position.z > 7.5)
+        {
+            transform.Rotate(new Vector3(0, 90, 0));
+            transform.position += moveDirection * Time.deltaTime;
+        }
+        else if (transform.position.x >= upperBoundX)
         {
             Destroy(gameObject);
         }
