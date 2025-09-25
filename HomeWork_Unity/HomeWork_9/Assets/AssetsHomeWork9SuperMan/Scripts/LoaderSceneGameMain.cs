5cb079742f6bb1362fd53bf887a9299a173cb497
Алексей <manay.ru@mail.ru>
2025-09-25 19:23:22 +1000

    Добавил кнопку выхода

---
+++ HomeWork_Unity/HomeWork_9/Assets/AssetsHomeWork9SuperMan/Scripts/LoaderSceneGameMain.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderSceneGameMain : MonoBehaviour
+{
+    public string nameScene;
+    public void SceneLoad()
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+}
