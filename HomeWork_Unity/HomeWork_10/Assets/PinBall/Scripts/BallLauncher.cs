4ba99b726207a660601b6534c6d27f00957b35e2
Алексей <manay.ru@mail.ru>
2025-10-30 22:22:57 +1000

    Ненужные файлы

--- HomeWork_Unity/HomeWork_10/Assets/PinBall/Scripts/BallLauncher.cs
+++ HomeWork_Unity/HomeWork_10/Assets/PinBall/Scripts/BallLauncher.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BallLauncher : MonoBehaviour
 {
+    public AudioClip audioClip;
     private Rigidbody rb;
 
+    private AudioSource audioSource;
+
     public float ballForce = 10f;
+    public float ballForceCaosule = 1.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        
+        audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
+        audioSource.Play();
         LaunchBall();
     }
 
@@ -21,7 +28,6 @@ public class BallLauncher : MonoBehaviour
     {
         
     }
-
     public void LaunchBall()
     {
         rb.AddForce(transform.forward * ballForce, ForceMode.Impulse);
@@ -31,7 +37,15 @@ public class BallLauncher : MonoBehaviour
     {
         if (other.gameObject)
         {
-            rb.AddForce(Vector3.forward * ballForce, ForceMode.Impulse); 
+            rb.AddForce(Vector3.forward * ballForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Board"))
+        {
+            rb.AddForce(Vector3.forward * ballForceCaosule, ForceMode.Impulse);
         }
     }
 }
