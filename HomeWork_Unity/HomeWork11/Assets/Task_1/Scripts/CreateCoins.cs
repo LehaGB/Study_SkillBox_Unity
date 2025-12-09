86af2b01acc3ea4b9ed6cba3075cdbd6e4b4248f
Алексей <manay.ru@mail.ru>
2025-12-09 15:24:05 +1000

    Изменил Background и немного скрипт

--- HomeWork_Unity/HomeWork11/Assets/Task_1/Scripts/CreateCoins.cs
+++ HomeWork_Unity/HomeWork11/Assets/Task_1/Scripts/CreateCoins.cs
@@ -4,35 +4,19 @@ using UnityEngine;
 
 public class CreateCoins : MonoBehaviour
 {
-    [SerializeField] private GameObject coinPrefab;
+    [SerializeField] private GameObject _coinPrefab;
 
-    private float _posX = -1.5f;
-    private float _posX2 = 2.3f;
-    private float _posY = 42f;
-    private float _posY2 = 3f;
-    private int _countCoinMax = 15;
-    private int _countCoinMin = 5;
+    private CreateCoinsPrefab _prefabCreator;
+
+
+    private void Awake()
+    {
+        _prefabCreator = new CreateCoinsPrefab();
+        _prefabCreator._coinPrefab = _coinPrefab;
+    }
 
     private void Start()
     {
-        RandomCreateCoins();
-    }
-
-
-    public void RandomCreateCoins()
-    {
-        int randomCountCoint = Random.Range(_countCoinMin, _countCoinMax);
-
-        for (int i = 0; i < randomCountCoint; i++)
-        {
-            float randomX = Random.Range(_posX, _posX2);
-            float randomY = Random.Range(_posY, _posY2);
-
-            Vector3 spawnPosition = transform.position + new Vector3(randomX, 0.1f, randomY);
-
-            GameObject newCoin = Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
-
-            newCoin.transform.SetParent(transform);
-        } 
+        _prefabCreator.CreatePrefab(transform);
     }
 }
