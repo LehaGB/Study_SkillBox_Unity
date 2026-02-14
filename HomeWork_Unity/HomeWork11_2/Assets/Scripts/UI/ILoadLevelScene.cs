7b56e29b33bfd035fd3aaf2a18fc6c1850140537
Алексей <manay.ru@mail.ru>
2026-02-14 14:23:27 +1000

    new project, add file .gitignore, add Zenject

---
+++ HomeWork_Unity/HomeWork11_2/Assets/Scripts/UI/ILoadLevelScene.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILoadLevelScene
+{
+    event Action <int> OnLoadLevel;
+    void LoadScene(int indexScene);
+    void LoadLevelButtonClicked(int levelIndex);
+
+    void Play();
+
+    void Exit();
+}
