b34665a6f75cf881d8f754aef06b2c227321f3f1
Алексей <manay.ru@mail.ru>
2026-02-15 12:13:29 +1000

    Delete

--- HomeWork_Unity/HomeWork11_2/Assets/WildBall/Scripts/UI/UIManager.cs
+++ HomeWork_Unity/HomeWork11_2/Assets/WildBall/Scripts/UI/UIManager.cs
@@ -9,40 +9,46 @@ public class UIManager : MonoBehaviour
 {
     [Inject] private ILoadLevelScene _loadLevelScene;
     [Inject] private TimeController _timeController;
+    [Inject] private AudioManager _audioManager;
 
     public void ButtonPlayClicked()
     {
-        _loadLevelScene.Play();
+        _loadLevelScene?.Play();
+        _audioManager?.SwitchMusic();
     }
 
     public void ButtonExitClicked()
     {
-        _loadLevelScene.Exit();
+        _loadLevelScene?.Exit();
     } 
     
 
     public void ButtonMenuClicked()
     {
-        _loadLevelScene.Menu();
+        _loadLevelScene?.Menu();
+        _audioManager?.PlayMusic();
     }
 
     public void ButtonBackClicked()
     {
-        _loadLevelScene.Back();
+        _loadLevelScene?.Back();
+        _audioManager?.SwitchMusic();
     }
 
     public void ButtonLoadLevelClicked(int indexScene)
     {
-        _loadLevelScene.LoadLevelButtonClicked(indexScene);
+        _loadLevelScene?.LoadLevelButtonClicked(indexScene);
     }
 
     public void ButtonPauseClicked()
     {
-        _timeController.SetPauseOn();
+        _timeController?.SetPauseOn();
+        _audioManager?.PauseMusic();
     }
 
     public void ButtonResumeClicked()
     {
-        _timeController.SetPauseOff();
+        _timeController?.SetPauseOff();
+        _audioManager?.ResumeMusic();
     }
 }
