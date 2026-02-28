0fb21706be3a7e51c93def2bbb7f95c9ac8d45a9
Алексей <manay.ru@mail.ru>
2026-03-01 08:25:40 +1000

    Delete

--- HomeWork_Unity/HomeWork11_2/Assets/WildBall/Scripts/Core/GameCore/GameManager.cs
+++ HomeWork_Unity/HomeWork11_2/Assets/WildBall/Scripts/Core/GameCore/GameManager.cs
@@ -1,25 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
-    private bool isPaused = false;
 
-    public void PauseGame() 
+    [Header("Managers")]
+    [SerializeField] private SceneLoader sceneLoader;
+    [SerializeField] private AudioManager audioManager;
+    [SerializeField] private UIManager uiManager;
+    [SerializeField] private TimerController timerController;
+
+
+    private void Awake()
     {
-        if (!isPaused)
-        {
-            Time.timeScale = 0;
-            isPaused = true;
-        }
-    }
-    public void ResumeGame()
-    {
-        if (!isPaused)
-        {
-            Time.timeScale = 1;
-            isPaused = false;
-        }
+        if (sceneLoader == null) sceneLoader = FindObjectOfType<SceneLoader>();
+        if (audioManager == null) audioManager = FindObjectOfType<AudioManager>();
+        if (uiManager == null) uiManager = FindObjectOfType<UIManager>();
+        if (timerController == null) timerController = FindObjectOfType<TimerController>();
+
+        DontDestroyOnLoad(this); 
+        DontDestroyOnLoad(sceneLoader);
+        DontDestroyOnLoad(audioManager);
     }
 }
