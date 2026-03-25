a5d3798794bcafd4d4a6738dd2b19f3241e62a45
Алексей <manay.ru@mail.ru>
2026-03-25 18:01:33 +1000

    Работа с канвасами

---
+++ HomeWork_Unity/HomeWork11_2/Assets/WildBall/Scripts/Utilitus/ScriptableOb.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableObject", menuName = "Create SO")]
+public class ScriptableOb : ScriptableObject
+{
+    public UIManager manager;
+}
