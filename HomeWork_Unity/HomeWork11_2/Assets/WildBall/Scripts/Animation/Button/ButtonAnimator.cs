d2bad128f29bf3b080c38fbc6a2b91da0da03ac8
Алексей <manay.ru@mail.ru>
2026-04-03 10:46:52 +1000

    Рефакторинг анимаций

---
+++ HomeWork_Unity/HomeWork11_2/Assets/WildBall/Scripts/Animation/Button/ButtonAnimator.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonAnimator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private Animator _animator;
+    private bool _isPointerInside = false;
+
+    public Button button;
+
+    public AudioMixerManager mixer;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        button = GetComponent<Button>();
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _animator.SetTrigger("Highlighted");
+        _isPointerInside = true;
+        mixer.PlayButtonClick();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _animator.SetTrigger("Disabled");
+        _isPointerInside = false;
+        mixer.StopButtonClick();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ResetToDefaultState();
+    }
+
+    private void ResetToDefaultState()
+    {
+       button.transform.localScale = Vector3.one;
+    }
+}
